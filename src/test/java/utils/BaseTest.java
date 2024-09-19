3ef915f04c7a5fb95a7b2a3dd424a4197fbf3860
niranjananandu <nanduniranjana@gmail.com>
2024-09-19 22:04:29 +0200

    added cross browser testing

--- src/test/java/utils/BaseTest.java
+++ src/test/java/utils/BaseTest.java
@@ -7,11 +7,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BaseTest {
 	
@@ -19,13 +19,27 @@ public class BaseTest {
 
 	public WebDriver setUp() throws IOException {	
 		Properties prop = new Properties();
-		
+		ChromeOptions options = new ChromeOptions();
+		Map<String,Object> prefs =  new HashMap<String,Object>();
+		prefs.put("download.default_directory", System.getProperty("user.dir")+"\\Downloaded Files");
+		options.setExperimentalOption("prefs", prefs);
+		String browser = System.getProperty("browser", "chrome");
+
 		if(driver==null) {
-			ChromeOptions options = new ChromeOptions();
-			Map<String,Object> prefs =  new HashMap<String,Object>();
-			prefs.put("download.default_directory", System.getProperty("user.dir")+"\\Downloaded Files");
-			options.setExperimentalOption("prefs", prefs);
-			driver =  new ChromeDriver(options);
+			switch (browser.toLowerCase()) {
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                driver = new EdgeDriver();
+                break;
+            case "chrome":
+            default:
+                driver = new ChromeDriver(options);
+                break;
+        }
+			
+//			driver =  new ChromeDriver(options);
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		}
 		
